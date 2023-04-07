190c9c3f70720eda81b880bdfbec038d4c407b4e
Gabriel <gabriel80546@gmail.com>
2023-04-07 19:07:13 -0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Olá Mundo");
+function enviarDados(tipo, valor) {
+    if (tipo === "dna") {
+        document.querySelector('#resultado').innerText = `dna: ${valor}`;
+    } else if (tipo === "rna") {
+        document.querySelector('#resultado').innerText = `rna: ${valor}`;
+    } else {
+        document.querySelector('#resultado').innerText = `ERRO`;
+    }
+}
