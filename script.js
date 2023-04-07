5bcf42aa85ae4fe9cd73ee05ab061eabbee740ab
Gabriel <gabriel80546@gmail.com>
2023-04-07 19:31:19 -0300

    bache

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function enviarDados(tipo, valor) {
         document.querySelector('#resultado').innerText = `ERRO`;
     }
 }
+
+fetch('webasmDna.wasm')
+  .then(response => response.arrayBuffer())
+  .then(bytes => WebAssembly.instantiate(bytes))
+  .then(results => {
+    // Atribui as funções do módulo WebAssembly a variáveis JavaScript
+    const add = results.instance.exports.add;
+    const subtract = results.instance.exports.subtract;
+    
+    // Usa as funções em sua página da web
+    alert(`UnicaVerdadeAbsoluta: ${UnicaVerdadeAbsoluta}`); // 5
+    // console.log(subtract(5, 3)); // 2
+});
